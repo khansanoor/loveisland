02187247b93607213acaba670984c8ab0ebd0b85
khansanoor <82741826+khansanoor@users.noreply.github.com>
2025-07-23 15:28:29 -0500

    Update matchmaker.py

--- matchmaker.py
+++ matchmaker.py
@@ -119,8 +119,7 @@ with st.form("participant_form", clear_on_submit=True):
             ]
         )
     with col1:
-        st.image(
-            "https://raw.githubusercontent.com/khansanoor/loveisland/main/huda%20dream%20date.jpg",
+        st.image("https://raw.githubusercontent.com/khansanoor/loveisland/main/huda%20dream%20date.jpg",
         use_container_width=True
         )
     
