822cd5c644e61f80c10626c40da8bb838d81ece5
khansanoor <82741826+khansanoor@users.noreply.github.com>
2025-07-23 14:19:29 -0500

    Update matchmaker.py

--- matchmaker.py
+++ matchmaker.py
@@ -5,12 +5,12 @@ import json # To parse the service account key from secrets
 
 # --- Configuration ---
 # Set the page configuration for better aesthetics
-st.set_page_config(
-    page_title="Love Island Matchmaker",
-    page_icon="🌴",
-    layout="centered",
-    initial_sidebar_state="expanded"
-)
+st.set_page_config()
+#     page_title="Love Island Matchmaker",
+#     page_icon= "🌴",
+#     layout="centered",
+#     initial_sidebar_state="expanded"
+# )
 
 # --- Google Sheets Integration ---
 
